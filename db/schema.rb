e680588dccdb9e2ba3a056c7764d24cad918e7cc
MaksimDudar <maksimdudar1@gmail.com>
2022-08-27 00:15:37 +0300

    migration of products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_26_200919) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_26_204123) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
